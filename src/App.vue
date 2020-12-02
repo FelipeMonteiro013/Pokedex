a916f597517b64c9d6ca542ab0d542ca41238968
Felipe Monteiro <felipe-dp@hotmail.com.br>
2020-12-01 23:59:55 -0300

    Generations

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,44 @@
       <h1 class="is-size-1">Pokedéx</h1>
       <hr />
       <div id="teste">
+        <button id="pesquisar" class="button is-info" @click="geracao(1)">
+          Gereção 1
+        </button>
+        <button id="pesquisar" class="button is-info" @click="geracao(2)">
+          Gereção 2
+        </button>
+        <button id="pesquisar" class="button is-info" @click="geracao(3)">
+          Gereção 3
+        </button>
+        <button id="pesquisar" class="button is-info" @click="geracao(4)">
+          Gereção 4
+        </button>
+        <button id="pesquisar" class="button is-info" @click="geracao(5)">
+          Gereção 5
+        </button>
+        <button id="pesquisar" class="button is-info" @click="geracao(6)">
+          Gereção 6
+        </button>
+        <button id="pesquisar" class="button is-info" @click="geracao(7)">
+          Gereção 7
+        </button>
+        <button id="pesquisar" class="button is-info" @click="geracao(8)">
+          Gereção 8
+        </button>
+      </div>
+      <div id="teste">
         <input
           class="input"
           type="text"
           placeholder="Pesquisar pelo nome"
           v-model="busca"
         />
-        <button id="pesquisar" class="button is-success" @click="buscar">Pesquisar</button>
-        <button id="pesquisar" class="button is-info" @click="buscar">Limpar</button>
+        <button id="pesquisar" class="button is-success" @click="buscar">
+          Pesquisar
+        </button>
+        <button id="pesquisar" class="button is-info" @click="limpar">
+          Limpar
+        </button>
       </div>
 
       <div v-for="(poke) in filteredPokemons" :key="poke.url">
@@ -30,22 +60,85 @@ export default {
   components: {
     Pokemon,
   },
-  methods:{
-    buscar: function(){
+  methods: {
+    buscar: function () {
       this.filteredPokemons = this.pokemons;
-      if (this.busca == '' || this.busca == ' ') {
-        this.filteredPokemons = this.pokemons
-        console.log(this.pokemons);
-      }else{
-        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name.indexOf(this.busca) > -1)
-        console.log(this.filteredPokemons);
+      if (this.busca == "" || this.busca == " ") {
+        this.filteredPokemons = this.pokemons;
+      } else {
+        this.filteredPokemons = this.pokemons.filter(
+          (pokemon) => pokemon.name.indexOf(this.busca) > -1
+        );
       }
-    }
+    },
+    limpar: function(){
+      this.busca = ""
+      this.filteredPokemons =  this.pokemons
+    },
+    geracao: function (value) {
+      if (value == 2) {
+        axios
+          .get("https://pokeapi.co/api/v2/pokemon?limit=100&offset=151")
+          .then((res) => {
+            this.pokemons = res.data.results;
+            this.filteredPokemons = res.data.results;
+          });
+      } else if (value == 3) {
+        axios
+          .get("https://pokeapi.co/api/v2/pokemon?limit=135&offset=251")
+          .then((res) => {
+            this.pokemons = res.data.results;
+            this.filteredPokemons = res.data.results;
+          });
+      } else if (value == 4) {
+        axios
+          .get("https://pokeapi.co/api/v2/pokemon?limit=107&offset=386")
+          .then((res) => {
+            this.pokemons = res.data.results;
+            this.filteredPokemons = res.data.results;
+          });
+      } else if (value == 5) {
+        axios
+          .get("https://pokeapi.co/api/v2/pokemon?limit=156&offset=493")
+          .then((res) => {
+            this.pokemons = res.data.results;
+            this.filteredPokemons = res.data.results;
+          });
+      } else if (value == 6) {
+        axios
+          .get("https://pokeapi.co/api/v2/pokemon?limit=72&offset=649")
+          .then((res) => {
+            this.pokemons = res.data.results;
+            this.filteredPokemons = res.data.results;
+          });
+      } else if (value == 7) {
+        axios
+          .get("https://pokeapi.co/api/v2/pokemon?limit=88&offset=721")
+          .then((res) => {
+            this.pokemons = res.data.results;
+            this.filteredPokemons = res.data.results;
+          });
+      } else if (value == 8) {
+        axios
+          .get("https://pokeapi.co/api/v2/pokemon?limit=89&offset=809")
+          .then((res) => {
+            this.pokemons = res.data.results;
+            this.filteredPokemons = res.data.results;
+          });
+      }else{
+        axios
+          .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+          .then((res) => {
+            this.pokemons = res.data.results;
+            this.filteredPokemons = res.data.results;
+          });
+      }
+    },
   },
-  computed:{
+  computed: {
     // resultadoBusca: function () {
     //   if (this.busca == '' || this.busca == ' ') {
-    //     return this.pokemons        
+    //     return this.pokemons
     //   }else{
     //     return this.pokemons.filter(pokemon => pokemon.name == this.busca);
     //   }
@@ -84,9 +177,10 @@ export default {
 #teste {
   display: flex;
   justify-content: space-around;
+  margin-top: 1%;
 }
 
-#pesquisar{
+#pesquisar {
   margin-left: 1%;
 }
 </style>
